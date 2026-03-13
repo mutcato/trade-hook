ee19e0216f88937a3dbf345f918c37c5bff21116
mutcato <mtokat1@gmail.com>
2026-03-13 17:17:02 +0300

    Format all

--- authentication/admin.py
+++ authentication/admin.py
@@ -9,21 +9,42 @@ User = get_user_model()
 class UserAdmin(BaseUserAdmin):
     """Admin configuration for custom User model."""
 
-    list_display = ['email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined']
-    list_filter = ['is_staff', 'is_superuser', 'is_active']
-    search_fields = ['email', 'first_name', 'last_name']
-    ordering = ['-date_joined']
+    list_display = [
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+        "date_joined",
+    ]
+    list_filter = ["is_staff", "is_superuser", "is_active"]
+    search_fields = ["email", "first_name", "last_name"]
+    ordering = ["-date_joined"]
 
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login',)}),
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login",)}),
     )
 
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
     )
--- trader/serializers.py
+++ trader/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 
 from authentication.serializers import UserSerializer
-from .models import HookSet, Hook, Trade
+
+from .models import Hook, HookSet, Trade
 
 
 class HookCreateUpdateSerializer(serializers.ModelSerializer):
@@ -9,7 +10,7 @@ class HookCreateUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Hook
-        fields = ['hook_set', 'created_by']
+        fields = ["hook_set", "created_by"]
 
 
 class HookSerializer(serializers.ModelSerializer):
@@ -17,7 +18,15 @@ class HookSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Hook
-        fields = ['id', 'hook_set', 'is_triggered', 'triggered_at', 'created_by', 'created_at', 'updated_at']
+        fields = [
+            "id",
+            "hook_set",
+            "is_triggered",
+            "triggered_at",
+            "created_by",
+            "created_at",
+            "updated_at",
+        ]
 
 
 class HookSetCreateUpdateSerializer(serializers.ModelSerializer):
@@ -25,7 +34,8 @@ class HookSetCreateUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = HookSet
-        fields = ['id', 'name', 'timewindow', 'created_by']
+        fields = ["id", "name", "timewindow", "created_by"]
+
 
 class HookSetSerializer(serializers.ModelSerializer):
     created_by = UserSerializer(read_only=True)
@@ -33,7 +43,15 @@ class HookSetSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = HookSet
-        fields = ['id', 'name', 'timewindow', 'hook_set', 'created_by', 'created_at', 'updated_at']
+        fields = [
+            "id",
+            "name",
+            "timewindow",
+            "hook_set",
+            "created_by",
+            "created_at",
+            "updated_at",
+        ]
 
 
 class TradeCreateUpdateSerializer(serializers.ModelSerializer):
@@ -41,7 +59,7 @@ class TradeCreateUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Trade
-        fields = ['id', 'name', 'pair', 'action', 'hookset', 'created_by']
+        fields = ["id", "name", "pair", "action", "hookset", "created_by"]
 
 
 class TradeSerializer(serializers.ModelSerializer):
@@ -50,4 +68,15 @@ class TradeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Trade
-        fields = ['id', 'name', 'pair', 'action', 'hookset', 'is_triggered', 'triggered_at', 'created_by', 'created_at', 'updated_at']
+        fields = [
+            "id",
+            "name",
+            "pair",
+            "action",
+            "hookset",
+            "is_triggered",
+            "triggered_at",
+            "created_by",
+            "created_at",
+            "updated_at",
+        ]
